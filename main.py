5aaba302b8e62cf1ad776dc768eea1061789a947
Timothy Crundall <tim.crundall@gmail.com>
2022-05-30 19:31:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import Widget
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class ActivityTrackerApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    ActivityTrackerApp().run()
