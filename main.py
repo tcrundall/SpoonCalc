a013e60576a8dc6d096240511980e58543c60873
Timothy Crundall <tim.crundall@gmail.com>
2022-06-03 14:34:58 +0200

    Redo time handling with datetime

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ Config.set('graphics', 'height', '830')
 
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.boxlayout import BoxLayout
 
-import datetime
-import json
-import os
+from datetime import datetime, timedelta
 import sqlite3
-import time
 
-DATA_DIR = 'data'
 DATABASE = 'spooncalc.db'
 
 LOW = 1
@@ -24,17 +20,33 @@ HI = 3
 
 
 class MainWidget(BoxLayout):
-    duration = 60
-    duration_min = NumericProperty(0)
-    duration_hr = NumericProperty(duration // 60)
+    """
+    Activity input
+    Use this widget to input an activity with the following properties:
+        - namm
+        - cognitive load
+        - physical load
+        - duration
+        - end time
+        - current energy level
 
-    end_time = time.localtime().tm_hour * 60
-    # print(f"{end_time=}")
-    end_time_min = NumericProperty(0)
-    end_time_hr = NumericProperty(end_time // 60)
-    # end_time_hr = end_time // 60
-    # print(end_time // 60)
-    # print(f"{int(end_time_hr)}")
+    Limitations/assumptions:
+        - can only input data for today
+    
+    Possible extensions:
+        - snap begin time to end of last event
+    """
+
+    duration_timedelta = timedelta(hours=1)
+    duration_display = StringProperty(
+        ":".join(str(duration_timedelta).split(':')[:2])
+    )
+
+    end_datetime = datetime.now().replace(minute=0, second=0, microsecond=0)
+    end_display = StringProperty(end_datetime.strftime("%H:%M"))
+
+    start_datetime = end_datetime - duration_timedelta
+
     button_ids = {
         'cog': {
             'cog_low',
@@ -72,100 +84,68 @@ class MainWidget(BoxLayout):
         super().__init__(**kwargs)
 
     def on_toggle(self, toggle, load):
+        """
+        Ensure that exactly one toggle for load `load` is down
+        """
         if toggle.state == "down":
             for id in self.button_ids[load]:
                 if self.ids[id] != toggle:
                     self.ids[id].state = "normal"
 
-    # def on_toggle_cog(self, toggle):
-    #     """
-    #     Ensure only one cognitive load toggle can
-    #     be down.
-    #     """
-    #     if toggle.state == 'down':
-    #         for id in self.cog_button_ids:
-    #             # import ipdb; ipdb.set_trace()
-    #             if self.ids[id] != toggle:
-    #                 self.ids[id].state = "normal"
-
-    # def on_toggle_phys(self, toggle):
-    #     """
-    #     Ensure only one physical load toggle can
-    #     be down.
-    #     """
-    #     if toggle.state == 'down':
-    #         for id in self.phys_button_ids:
-    #             # import ipdb; ipdb.set_trace()
-    #             if self.ids[id] != toggle:
-    #                 self.ids[id].state = "normal"
-
     def on_toggle_duration(self, widget):
-        duration = 0
+        """
+        Recalculate activity duration on each new toggle
+        """
+        duration_timedelta = timedelta()
         for id, val in self.duration_toggles.items():
             if self.ids[id].state == "down":
-                duration += val
-        self.duration_min = duration % 60
-        self.duration_hr = duration // 60
+                duration_timedelta += timedelta(minutes=val)
 
-    # def on_dur_text_validate(self, widget):
-    #     duration = None
-    #     # assume x:xx format
-    #     duration = widget.text.split(':')
+        self.duration_timedelta = duration_timedelta
+        self.duration_display = \
+            ':'.join(str(self.duration_timedelta).split(':')[:2])
 
-    #     try:
-    #         duration = int(widget.text)
-    #     except ValueError:
-    #         pass
-    #     if duration is None
+        print(f"{self.duration_timedelta=!s}")
 
     def on_end_time_press(self, button: Button):
-        self.end_time += self.end_time_buttons[button.text]
-        self.end_time %= (24 * 60)
+        """
+        Inc-/decrement the end time based on pressed button and update display
 
-        self.end_time_min = self.end_time % 60
-        self.end_time_hr = self.end_time // 60
-
-    # def on_toggle_energy(self, toggle: ToggleButton):
-    #     """
-    #     Ensure only one physical load toggle can
-    #     be down.
-    #     """
-    #     print(f"{toggle.parent.text=}")
-    #     print(f"{toggle.parent.ids=}")
-    #     print(f"{self.ids=}")
-    #     print(f"{toggle.text=}")
-    #     print(f"{toggle.proxy_ref=}")
-    #     # print(f"{toggle.=}")
-    #     if toggle.state == 'down':
-    #         for id in self.energy_button_ids:
-    #             # import ipdb; ipdb.set_trace()
-    #             if self.ids[id] != toggle:
-    #                 self.ids[id].state = "normal"
+        The value to be inc-/decremented is inferred from the 
+        button's text.
+        """
+        self.end_datetime += timedelta(
+            minutes=self.end_time_buttons[button.text]
+        )
+        self.end_display = self.end_datetime.strftime("%H:%M")
 
     def insert_into_database(self):
+        """
+        Insert current inputted data into the database
+        """
         conn = sqlite3.connect(DATABASE)
         c = conn.cursor()
-        c.execute("INSERT INTO activities VALUES (:name), (:cogload), (:end)",
+        c.execute("INSERT INTO activities VALUES (:end), (:duration), " +
+                  "(:name), (:cogload), (:physload), (:energy)",
                   {
+                      'end': str(self.end_datetime),
+                      'duration': str(self.duration_timedelta),
                       'name': self.ids['activity_name'].text,
                       'cogload': self.get_level('cog'),
-                      'end': self.end_time,
+                      'physload': self.get_level('phys'),
+                      'energy': self.get_level('energy')
                   })
         conn.commit()
         c.execute("SELECT * FROM activities")
         contents = c.fetchall()
         conn.commit()
         print(contents)
-        
+
         conn.close()
 
     def on_save_press(self):
-        save_dict = self.build_save_dict()
-        filename = os.path.join(DATA_DIR, self.generate_ativity_name())
-        with open(filename, 'w') as fp:
-            save_json = json.dump(save_dict, fp)
-
         self.insert_into_database()
+        # Change view screen somehow...
 
     def get_level(self, load):
         for id in self.button_ids[load]:
@@ -173,27 +153,19 @@ class MainWidget(BoxLayout):
                 return id.split('_')[-1]
         return -1
 
-    def build_save_dict(self):
-        activity_log = {}
-        for load in ['cog', 'phys', 'energy']:
-            activity_log[load] = self.get_level(load)
-        activity_log['duration'] = self.duration
-        activity_log['end_time'] = self.end_time
-        activity_log['activity_name'] = self.ids['activity_name'].text
-        return activity_log
+    # def generate_ativity_name(self):
+    #     """
+    #     date, time, name
+    #     """
+    #     now = time.localtime()
+    #     now = datetime.now().replace(microsecond=0)
+    #     filename = str(now).replace(' ', '_').replace(':','-')
 
-    def generate_ativity_name(self):
-        """
-        date, time, name
-        """
-        now = time.localtime()
-        filename = f"{now.tm_year}_{now.tm_mon:02}_{now.tm_mday:02}_" +\
-                   f"{now.tm_hour:02}_{now.tm_min:02}_{now.tm_sec:02}"
-        # TODO: set `activity name` to a default string
-        if self.ids['activity_name'].text != "Activity name":
-            filename += f"_{self.ids['activity_name'].text}"
-        filename += '.json'
-        return filename
+    #     # TODO: set `activity name` to a default string
+    #     if self.ids['activity_name'].text != "Activity name":
+    #         filename += f"_{self.ids['activity_name'].text}"
+    #     filename += '.json'
+    #     return filename
 
 
 class SpoonCalcApp(App):
@@ -203,9 +175,9 @@ class SpoonCalcApp(App):
 
         c = conn.cursor()
 
-        # c.execute("""CREATE TABLE if not exists activities(
-        c.execute("DROP TABLE if exists activities")
-        c.execute("""CREATE TABLE activities(
+        # c.execute("DROP TABLE if exists activities")
+        # c.execute("""CREATE TABLE activities(
+        c.execute("""CREATE TABLE if not exists activities(
             name text
             cogload text 
             endtime text
